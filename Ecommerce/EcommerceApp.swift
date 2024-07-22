3d59ba4eb5137d4d9bf93c65472c7d4df992f26d
priyashan22 <shanmugapriyam@presidio.com>
2024-07-22 13:59:13 +0530

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Shanmuga Priya M on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
